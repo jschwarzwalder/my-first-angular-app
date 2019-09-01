36235289d00dde1856f6f77a75b3cec4e2426a25
Jami Schwarzwalder <jamisch@amazon.com>
2019-08-31 21:08:11 -0700

    switching to using inline styles

--- my-first-angular-app/src/app/success/success.component.ts
+++ my-first-angular-app/src/app/success/success.component.ts
@@ -2,8 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-success',
-  templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  template: `
+  <div class="row">
+    <div class="col-sm-10 col-sm-offset-1">
+        <p class="alert alert-success" role="alert"><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+         Success works!</p>
+
+    </div>
+  </div>
+  `,
+  styles: [`
+    p {
+      padding: 20px;
+      color: darkgreen;
+      background-color: palegreen;
+      border: 1px solid green;
+    }
+  `]
 })
 export class SuccessComponent implements OnInit {
 
